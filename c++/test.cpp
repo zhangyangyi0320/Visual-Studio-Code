9052ea1c759a942c93244f9ce8c851c610b6d9aa
zhangyangyi <15378401+zhangyangyi0320@user.noreply.gitee.com>
2025-01-20 16:48:16 +0800

    1

--- c++/test.cpp
+++ c++/test.cpp
@@ -3,22 +3,35 @@
 using namespace std;
 
 int main() {
-    int m;
-    cin >> m;
-    vector<vector<int>> w(m, vector<int>(3));
-    for (int i = 0; i < m; ++i) {
-        cin >> w[i][0] >> w[i][1] >> w[i][2];
+    int C, n;
+    cin >> C >> n;
+    vector<int> w1(n);
+    vector<int> v1(n);
+    vector<int> c1(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> w1[i] >> v1[i] >> c1[i];
     }
-    int k;
-    cin >> k;
-    vector<int> dp(k + 1, 0);
-    for (const auto& w : w) {
-        int p = w[1];
-        int d = w[2];
-        for (int j = p; j <= k; ++j) {
-            dp[j] = max(dp[j], dp[j - p] + d);
+    vector<int> dp(C + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        int w = w1[i];
+        int v = v1[i];
+        int c = c1[i];
+        for (int k = 1; k <= c; k << 1) {
+            int w2 = k * w;
+            int v2 = k * v;
+            for (int j = C; j >= w2; --j) {
+                dp[j] = max(dp[j], dp[j - w2] + v2);
+            }
+            c -= k;
+        }
+        if (c > 0) {
+            int w3 = c * w;
+            int v3 = c * v;
+            for (int j = C; j >= w3; --j) {
+                dp[j] = max(dp[j], dp[j - w3] + v3);
+            }
         }
     }
-    cout << dp[k] << endl;
+    cout << dp[C] << endl;
     return 0;
 }
