7685a7b0b72b2bcaa90a1668b7f99f5e4d1b8523
zhangyangyi <15378401+zhangyangyi0320@user.noreply.gitee.com>
2025-01-20 16:12:50 +0800

    0

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,67 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int n, m;
-int arr[100005];
-struct node
-{
-    double x;
-    double y;
-    double z;
-};
-node a[2005];
-node lss[500005];
-bool cmp(node x, node y)
-{
-    return x.z < y.z;
-}
-int find(int x)
-{
-    if (x == arr[x])
-        return x;
-    return arr[x] = find(arr[x]);
-}
-void hb(int x, int y)
-{
-    int xx = find(x);
-    int yy = find(y);
-    arr[xx] = yy;
-}
-int main()
-{
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        arr[i] = i;
+
+int main() {
+    int m;
+    cin >> m;
+    vector<vector<int>> w(m, vector<int>(3));
+    for (int i = 0; i < m; ++i) {
+        cin >> w[i][0] >> w[i][1] >> w[i][2];
     }
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a[i].x >> a[i].y;
-    }
-    int t = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j < i; j++)
-        {
-            t++;
-            lss[t].x = i;
-            lss[t].y = j;
-            lss[t].z = sqrt((a[i].x - a[j].x) * (a[i].x - a[j].x) + (a[i].y - a[j].y) * (a[i].y - a[j].y));
+    int k;
+    cin >> k;
+    vector<int> dp(k + 1, 0);
+    for (const auto& w : w) {
+        int p = w[1];
+        int d = w[2];
+        for (int j = p; j <= k; ++j) {
+            dp[j] = max(dp[j], dp[j - p] + d);
         }
     }
-    int w = 0;
-    sort(lss + 1, lss + 1 + t, cmp);
-    for (int i = 1; i <= t; i++)
-    {
-        if (find(lss[i].x) != find(lss[i].y))
-        {
-            hb(lss[i].x, lss[i].y);
-            w++;
-            if (w == n - m)
-            {
-                printf("%.2f", lss[i].z);
-                return 0;
-            }
-        }
-    }
+    cout << dp[k] << endl;
     return 0;
 }
