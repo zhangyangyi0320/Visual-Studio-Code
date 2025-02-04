45f632d1b707ed0beb105614166663ec121da076
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-02-04 14:26:53 +0800

    a

--- c++/0.cpp
+++ c++/0.cpp
@@ -1,1 +1,25 @@
 #include <wx/wx.h>
+
+class MyFrame: public wxFrame
+{
+public:
+    MyFrame(const wxString& title)
+        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(300, 200))
+    {
+        wxPanel *panel = new wxPanel(this, wxID_ANY);
+        wxStaticText *helloLabel = new wxStaticText(panel, wxID_ANY, "HELLOWORLD", wxPoint(100, 50));
+    }
+};
+
+class MyApp: public wxApp
+{
+public:
+    virtual bool OnInit()
+    {
+        MyFrame *frame = new MyFrame("Hello wxWidgets");
+        frame->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
