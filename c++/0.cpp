752b88d5bdc619c1ec57dfbb8aa3a4c6ef271d24
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-02-04 17:15:18 +0800

    a

--- c++/0.cpp
+++ c++/0.cpp
@@ -1,25 +1,38 @@
 #include <wx/wx.h>
 
-class MyFrame: public wxFrame
-{
+class MyApp : public wxApp {
 public:
-    MyFrame(const wxString& title)
-        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(300, 200))
-    {
-        wxPanel *panel = new wxPanel(this, wxID_ANY);
-        wxStaticText *helloLabel = new wxStaticText(panel, wxID_ANY, "HELLOWORLD", wxPoint(100, 50));
-    }
+    virtual bool OnInit();
 };
 
-class MyApp: public wxApp
-{
+class MyFrame : public wxFrame {
 public:
-    virtual bool OnInit()
-    {
-        MyFrame *frame = new MyFrame("Hello wxWidgets");
-        frame->Show(true);
-        return true;
-    }
+    MyFrame(const wxString& title);
+
+private:
+    void OnExit(wxCommandEvent& event);
 };
 
 wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    MyFrame *frame = new MyFrame("HELLOWORLD");
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title) {
+    wxPanel *panel = new wxPanel(this, wxID_ANY);
+    wxStaticText *label = new wxStaticText(panel, wxID_ANY, "HELLOWORLD", wxPoint(10, 10));
+    
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(label, 0, wxALL, 10);
+    panel->SetSizer(sizer);
+
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+
+void MyFrame::OnExit(wxCommandEvent& event) {
+    Close(true);
+}
