813e966d24d4bd63d4b5682f3c3235e94644c4d5
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-02-06 22:47:01 +0800

    a

--- c++/0.cpp
+++ c++/0.cpp
@@ -1,38 +1,8 @@
-#include <wx/wx.h>
-
-class MyApp : public wxApp {
-public:
-    virtual bool OnInit();
-};
-
-class MyFrame : public wxFrame {
-public:
-    MyFrame(const wxString& title);
-
-private:
-    void OnExit(wxCommandEvent& event);
-};
-
-wxIMPLEMENT_APP(MyApp);
-
-bool MyApp::OnInit() {
-    MyFrame *frame = new MyFrame("HELLOWORLD");
-    frame->Show(true);
-    return true;
-}
-
-MyFrame::MyFrame(const wxString& title)
-    : wxFrame(NULL, wxID_ANY, title) {
-    wxPanel *panel = new wxPanel(this, wxID_ANY);
-    wxStaticText *label = new wxStaticText(panel, wxID_ANY, "HELLOWORLD", wxPoint(10, 10));
-    
-    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(label, 0, wxALL, 10);
-    panel->SetSizer(sizer);
-
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-}
-
-void MyFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    freopen("0.cpp","r",stdin);
+    long long a;
+    cin >> a;
+    fclose(stdout);
 }
