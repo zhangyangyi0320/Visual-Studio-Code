ab85f12a0ce9d81fe916d70d484211dadcf5db30
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-02-13 19:14:07 +0800

    a

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,9 +2,35 @@
 
 using namespace std;
 
+int dp[20][210];
+
+struct node{
+    int w;
+    int c;
+};
+
 int main() {
-    string a;
-    cin >> a;
-    cout << a << endl;
+
+    int V,N,T;
+    cin>>V>>N>>T;
+    vector<node> ve[20];
+    for(int i=1;i<=N;i++){
+        int W,C,P;
+        cin>>W>>C>>P;
+        ve[P].push_back({W,C});
+    }
+
+    for(int i=1;i<=T;i++){
+        for(int j=0;j<=V;j++){
+            dp[i][j]=dp[i-1][j];
+            for(int k=0;k<ve[i].size();k++){
+                if(j>=ve[i][k].w)
+                    dp[i][j]=max(dp[i][j],dp[i-1][j-ve[i][k].w]+ve[i][k].c);
+            }
+        }
+    }
+
+    cout<<dp[T][V]<<endl;
+
     return 0;
 }
