49ba7450a9e4459034e0e7dedc094a93701de348
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-02-16 10:27:23 +0800

    a

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,35 +2,37 @@
 
 using namespace std;
 
-int dp[6001];
+long long dp[1010];
 
 struct node
 {
-    int a,b;
+    int b,a;
 };
 
 int main()
 {
     int n, m;
     cin >> m >> n;
-    vector<node> ve(110);
+    vector<node> ve[110];
     for (int i = 1; i <= n; i++)
     {
         int a,b,c;
         cin >> a >> b >> c;
-        ve[c].a = a;
-        ve[c].b = b;
+        ve[c].push_back({a,b});
     }
-    for (int i = 1; i <= m; i++)
+    for (int i = 1; i <= 100; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = m; j >= 0; j--)
         {
-            if (i >= ve[j].b)
+            for (int k = 0; k < ve[i].size(); k++)
             {
-                dp[i] = max(dp[i], dp[i-ve[j].b]+ve[j].a);
+                if (j >= ve[i][k].b)
+                {
+                    dp[j] = max(dp[j], dp[j-ve[i][k].b]+ve[i][k].a);
+                }
             }
         }
     }
-    cout << dp[m] << endl;
+    cout << dp[m];
     return 0;
 }
