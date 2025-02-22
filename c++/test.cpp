7f9be079e762de4849303ecd4dc3fdf861c71ec4
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-02-22 20:54:10 +0800

    a

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,23 +2,26 @@
 
 using namespace std;
 
-int dp[110][110];
-int a[110];
+int dp[210][210];
+int a[210];
 
 int main()
 {
     int n;
     cin >> n;
-    vector<int> ve(110);
+    vector<int> ve(210);
     for (int i = 1; i <= n; i++)
     {
         cin >> ve[i];
+        ve[i + n] = ve[i];
+    }
+    for (int i = 1; i <= (n << 1); i++)
+    {
         a[i] = a[i - 1] + ve[i];
     }
-
     for (int l = 2; l <= n; l++)
     {
-        for (int i = 1, j = l; j <= n; i++, j++)
+        for (int i = 1, j = l; j <= (n << 1); i++, j++)
         {
             dp[i][j] = INT_MAX;
             for (int k = i; k < j; k++)
@@ -27,7 +30,11 @@ int main()
             }
         }
     }
-
-    cout << dp[1][n];
+    int ans = INT_MAX;
+    for(int i=1;i<=n;i++)
+    {
+        ans = min(ans,dp[i][i+n-1]);
+    }
+    cout << ans;
     return 0;
 }
