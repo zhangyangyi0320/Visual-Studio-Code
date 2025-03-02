164fea29d76f55dd9f9687d283d6cc1f2b2de010
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-03-02 12:01:39 +0800

    a

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,65 +2,40 @@
 
 using namespace std;
 
+int sum = 0, n;
+
 struct node
 {
-    string n;
-    string p;
-    int c;
-} a[110];
+    int dps, hp;
+    double t;
+} a[30];
 
 bool cmp(node a, node b)
 {
-    if (a.p != b.p)
-    {
-        return a.p < b.p;
-    }
-    else
-    {
-        return a.n < b.n;
-    }
+    return a.t > b.t;
 }
 
 int main()
 {
-    int n;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        int m;
-        cin >> m;
-        for (int j = 0; j < m; j++)
-        {
-            cin >> a[j].n >> a[j].p >> a[j].c;
-        }
-        sort(a, a + m, cmp);
-        string q = a[0].n, p = a[0].p;
-        int k = a[0].c;
-        cout << p << endl;
-        for (int j = 1; j < m; j++)
-        {
-            if (a[j].p != p)
-            {
-                cout << "   |----" << q << "(" << k << ")" << endl;
-                q = a[j].n;
-                p = a[j].p;
-                k = a[j].c;
-                cout << p << endl;
-            }
-            else if (a[j].n != q)
-            {
-                cout << "   |----" << q << "(" << k << ")" << endl;
-                q = a[j].n;
-                k = a[j].c;
-            }
-            else
-            {
-                k += a[j].c;
-            }
-            
-        }
-        cout << "   |----" << q << "(" << k << ")" << endl;
-        cout << endl;
+        cin >> a[i].dps >> a[i].hp;
+        a[i].t = a[i].dps * 1.0 / a[i].hp;
+        sum += a[i].dps;
     }
+    sort(a, a + n, cmp);
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int m = a[i].hp;
+        while (m > 0)
+        {
+            m--;
+            ans += sum;
+        }
+        sum -= a[i].dps;
+    }
+    cout << ans;
     return 0;
 }
