a6d0685da42664247b52ae600dc25afccf34bb99
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-03-02 09:59:24 +0800

    a

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,39 +2,50 @@
 
 using namespace std;
 
-const int N = 6E3+10;
-vector<int> ve[N];
-int h[N], vis[N], n,f[N][2];
-void dfs(int x) {
-    f[x][0] = 0;
-    f[x][1] = h[x];
-    for (int j = 0; j < ve[x].size(); j++) {
-        int y = ve[x][j];
-        dfs(y);
-        f[x][0] += max(f[y][0], f[y][1]);
-        f[x][1] += f[y][0];
+const int MAXN = 2e5+10;
+vector<int> ve[MAXN];
+int dp[MAXN][2];
+
+void dfs(int u, int a) {
+    dp[u][0] = 0;
+    dp[u][1] = INT_MAX;
+    int cnt = 0;
+
+    for (int v : ve[u]) {
+        if (v == a) continue;
+        dfs(v, u);
+        cnt++;
+
+        dp[u][0] += min(dp[v][0], dp[v][1]);
+
+        if (dp[v][1] != INT_MAX) {
+            dp[u][1] = min(dp[u][1], dp[v][1] + dp[u][0] - min(dp[v][0], dp[v][1]));
+        }
+    }
+
+    if (cnt < 2) {
+        dp[u][1] = INT_MAX;
     }
 }
 
 int main() {
+    int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> h[i];
+
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        ve[u].push_back(v);
+        ve[v].push_back(u);
     }
-    for (int i = 1; i < n; i++) {
-        int l,k;
-        cin >> l >> k;
-        ve[k].push_back(l);
-        vis[l] = 1;
+
+    dfs(1, -1);
+
+    if (dp[1][1] == INT_MAX) {
+        cout << -1 << endl;
+    } else {
+        cout << dp[1][1] << endl;
     }
-    int root;
-    for (int i = 1; i <= n; i++) {
-        if (!vis[i]) {
-            root = i;
-            break;
-        }
-    }
-    dfs(root);
-    cout << max(f[root][0], f[root][1]);
+
     return 0;
 }
