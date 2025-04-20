47ea204dbed3a7a60a1d6cb5b02bf37ec17fbced
zhangyangyi0320 <gebilaowang0320@outlook.com>
2025-04-20 10:28:45 +0800

    a

--- c++/U9/T4/main.cpp
+++ c++/U9/T4/main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
-    
+    int m, n;
+    cin >> m >> n;
+    vector<pair<int, pair<int, int>>> e(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin>>e[i].second.first>>e[i].second.second>>e[i].first;
+    }
+    sort(e.begin(), e.end());
     return 0;
 }
